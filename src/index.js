acd1cefe923ad7f221bab90a8bdabba2269780f2
Heorgii <rushchakheorgii@gmail.com>
2022-09-20 19:11:35 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const listEl= document.querySelector('.country-list');
 const infoEl= document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+const cleanMarkup = ref => (ref.innerHTML = '');
 
 const inpHendler = e =>{
     const inpText = e.target.value.trim();
@@ -24,12 +25,44 @@ const inpHendler = e =>{
             Notify.info("Too many matches found. Please enter a more specific name.");
             return;
         }
+        renderMarkup(data);
     })
     .catch(err =>{
         cleanMarkup(listEl);
         cleanMarkup(infoEl);
-        Notify.failure('')
+        Notify.failure("Oops, there is no country with that name");
     });
-};
+}
+
+const renderMarkup = data => {
+    if(data.length ===1){
+        cleanMarkup(listEl);
+        const markupInfo = createInfoMarkup(data);
+        listEl.innerHTML=markupInfo;
+    }
+    else{
+        cleanMarkup(infoEl);
+        const markupList = createListMarkup(data);
+        listEl.innerHTML = markupList;
+    }
+}
+
+const createListMarkup = data => {
+    return data
+    .map(({name, flags}) => 
+        `<li><img src = "${flags.png}" alt= "${name.official}" width = "60" height = "70">${name.official} </li>`,
+    )
+    .join('');
+}
+
+const createInfoMarkup = data =>{
+    return data 
+    .map(({name, capital, population, flags, languages}) =>
+    `<h1><img src = "${flags.png}" alt= "${name.official}" width = "40" height = "40">${name.official}</h1>
+    <p>Capital: ${capital}</p>
+    <p>Population: ${population}</p>
+    <p>Languages: ${Object.values(languages)}</p>`
+    );
+}
 
 inpEl.addEventListener('input', debounce(inpHendler, DEBOUNCE_DELAY));
