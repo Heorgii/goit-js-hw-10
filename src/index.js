8e53fd2b700054eca8d1486e903bde029c518508
Heorgii <rushchakheorgii@gmail.com>
2022-09-20 18:35:17 +0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const inpEl = document.getElementById('search-box');
+const listEl= document.querySelector('.country-list');
+const infoEl= document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+const inpHendler = e =>{
+    const inpText = e.target.value.trim();
+
+    if(!inpText){
+        cleanMarkup(listEl);
+        cleanMarkup(infoEl);
+        return;
+    }
+
+    fetchCountries(inpText).then(data =>{
+        console.log(data);
+        if(data.length > 10){
+            Notify.info("Too many matches found. Please enter a more specific name.");
+            return;
+        }
+    })
+    .catch(err =>{
+        cleanMarkup(listEl);
+        cleanMarkup(infoEl);
+        Notify.failure('')
+    });
+};
+
+inpEl.addEventListener('input', debounce(inpHendler, DEBOUNCE_DELAY));
